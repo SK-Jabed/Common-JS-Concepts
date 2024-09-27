b47fb503f1228709e435f6c695f5235ba526ef7c
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-28 00:45:03 +0600

    Triple JS File Done of This REpository

---
+++ triple.js
@@ -0,0 +1,14 @@
+// Always use ===
+// Equal Comparison Doesn't Work for Non-primitive
+
+const first = [45, 54, 98];
+const second = [45, 54, 98];
+
+if(first === second){
+    console.log("values are equal")
+}
+else{
+    console.log("values are not equal");
+}
+
+// Type Coercion or Type Conversion or Type casting
